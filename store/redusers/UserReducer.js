6da9298851d84f3cfc474cc0bb0a3aa42da7c6cf
Evgen Kozenkov <klon19892@gmail.com>
2021-11-22 13:41:01 +0300

    Create redux folderes

---
+++ store/redusers/UserReducer.js
@@ -0,0 +1,16 @@
+
+const initialState = {
+  user: {
+    isAuth: true,
+    name: '',
+  },
+};
+
+const listState = (state = initialState, action) => {
+  switch (action.type) {
+    
+    default: return state;
+  }
+};
+
+export default listState;
